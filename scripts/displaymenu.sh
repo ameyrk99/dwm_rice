1c73c28def6a3213cdaad00a16581b78a1d311f6
Amey Khairnar <ameykhairnar99@gmail.com>
2019-06-02 14:16:25 -0500

    Magenta; Rock svg; walls

--- scripts/displaymenu.sh
+++ scripts/displaymenu.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-case "$(echo -e "Laptop\nMonitor\nDuplicate\nMonitorRight\nMonitorLeft" | dmenu \
+case "$(echo -e "Laptop\nMonitor\nDuplicate\nMonitorDown\nMonitorUp" | dmenu \
     -fn "IBM 3270:size=14" \
     -nb "${COLOR_BACKGROUND:-#292d3e}" \
     -nb "${COLOR_BACKGROUND:-#292d3e}" \
     -nf "${COLOR_DEFAULT:-#eceff1}" \
     -sf "${COLOR_HIGHLIGHT:-#ffffff}" \
-    -sb "#c3e88d" \
+    -sb "#c792ea" \
     -i -p \
     "Display:" )" in
         Laptop) exec xrandr --output DP-1 --off --output eDP-1 --auto;;
         Monitor) exec xrandr --output DP-1 --auto --output eDP-1 --off;;
         Duplicate) exec xrandr --output DP-1 --auto --output eDP-1 --auto;;
-        MonitorRight) exec xrandr --output DP-1 --auto --output eDP-1 --left-of DP-1;;
-        MonitorLeft) exec xrandr --output DP-1 --auto --output eDP-1 --right-of DP-1;;
+        MonitorDown) exec xrandr --output DP-1 --auto --output eDP-1 --below DP-1;;
+        MonitorUp) exec xrandr --output DP-1 --auto --output eDP-1 --above DP-1;;
 esac
 
